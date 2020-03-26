a9598aff09250c9b6113dcd51c00fbd6c33bf62b
Luiz Dores <luizhenrique.pvh2000@gmail.com>
2020-03-26 15:01:57 +0000

    sdm632-common: Disable notification led while charging

--- light/Light.cpp
+++ light/Light.cpp
@@ -118,7 +118,6 @@ static void handleNotification(const LightState& state) {
 
 static std::map<Type, std::function<void(const LightState&)>> lights = {
     {Type::BACKLIGHT, handleBacklight},
-    {Type::BATTERY, handleNotification},
     {Type::NOTIFICATIONS, handleNotification},
     {Type::ATTENTION, handleNotification},
 };
